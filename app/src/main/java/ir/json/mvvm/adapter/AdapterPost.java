9a99a3e6dda7d1100069a7e3a4f66b11584c63ca
saeidkazemi <s.kazemi1373@yahoo.com>
2020-08-28 22:10:16 +0430

    change dataBinding to viewBinding

--- app/src/main/java/ir/json/mvvm/adapter/AdapterPost.java
+++ app/src/main/java/ir/json/mvvm/adapter/AdapterPost.java
@@ -1,12 +1,14 @@
 package ir.json.mvvm.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -17,23 +19,29 @@ import ir.json.mvvm.model.Post;
 
 public class AdapterPost extends RecyclerView.Adapter<AdapterPost.viewHolder> {
     private List<Post> posts;
-    ClickPostItem clickPostItem;
-
-    public AdapterPost(List<Post> posts,ClickPostItem clickPostItem) {
+    private ClickPostItem clickPostItem;
+    private Context mContext;
+    private ItemPostBinding binding;
+    public AdapterPost(List<Post> posts, ClickPostItem clickPostItem, Context mContext) {
         this.posts=posts;
         this.clickPostItem=clickPostItem;
+        this.mContext=mContext;
     }
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new viewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_post,parent,false));
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        binding = ItemPostBinding.inflate(inflater,parent,false);
+        return new viewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull viewHolder holder, int position) {
         final Post post = posts.get(position);
-        holder.binding.setPost(post);
+        Glide.with(mContext).load(posts.get(position).getImage())
+                .into(holder.binding.image);
+        holder.binding.name.setText(posts.get(position).getName());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +56,7 @@ public class AdapterPost extends RecyclerView.Adapter<AdapterPost.viewHolder> {
     }
 
     static class viewHolder extends RecyclerView.ViewHolder {
-        ItemPostBinding binding;
+        private ItemPostBinding binding;
         public viewHolder(@NonNull ItemPostBinding binding) {
             super(binding.getRoot());
             this.binding=binding;
--- app/src/main/java/ir/json/mvvm/view/activity/MainActivity.java
+++ app/src/main/java/ir/json/mvvm/view/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package ir.json.mvvm.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -24,30 +23,27 @@ import ir.json.mvvm.model.Post;
 import ir.json.mvvm.viewmodel.PostViewModel;
 
 public class MainActivity extends AppCompatActivity implements ClickPostItem {
-    RecyclerView recyclerview;
-    ProgressWheel progressWheel;
     ActivityMainBinding binding;
     AdapterPost adapterPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        recyclerview=binding.recyclerView;
-        progressWheel=binding.progressWheel;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         PostViewModel postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
         LiveData<List<Post>> listMutableLiveData=postViewModel.getPostResponseLiveData();
         listMutableLiveData.observe(this, new Observer<List<Post>>() {
             @Override
             public void onChanged(List<Post> posts) {
-                recyclerview.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                adapterPost = new AdapterPost(posts,MainActivity.this::clickPostItem);
-                recyclerview.setAdapter(adapterPost);
+                binding.recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                adapterPost = new AdapterPost(posts,MainActivity.this::clickPostItem,MainActivity.this);
+                binding.recyclerView.setAdapter(adapterPost);
             }
         });
         postViewModel.getPostRepository().progressState.observe(this, new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
-                if (!aBoolean)progressWheel.setVisibility(View.GONE);
+                if (!aBoolean)binding.progressWheel.setVisibility(View.GONE);
             }
         });
     }
